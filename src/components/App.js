d954a224d41ec08daecf4b46b7fce439f6b3f955
Atif Khan <29atiffkhann@gmail.com>
2021-05-23 16:53:45 +0530

    corrected

--- src/components/App.js
+++ src/components/App.js
@@ -2,59 +2,70 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 const App = (props) => {
-  const [slideind, setSlideInd] = useState(0);
+  const [slideIndex, setSlideIndex] = useState(0);
   const [slide, setSlide] = useState(props.slides[0]);
-  const [restartSlide, setRestartSlide] = useState(true);
-  const [prevSlide, setPrevSlide] = useState(true);
-  const [nextSlide, setNextSlide] = useState(false);
-  function next() {
-    setSlideInd(slideind + 1);
-    setSlide(props.slides[slideind + 1]);
+  const [prev, setPrev] = useState(true);
+  const [next, setNext] = useState(false);
+  const [restart, setRestart] = useState(true);
 
-    if(slideind===3){
-      setNextSlide(true)
+  function nextSlide() {
+   
+    console.log(slideIndex)
+    setSlideIndex(slideIndex + 1);
+    setSlide(props.slides[slideIndex + 1]);
+    if (slideIndex === 3) {
+      setNext(true);
     }
-    
-    if(slideind>-1){
-      setPrevSlide(false)
-      setRestartSlide(false)
+    if (slideIndex > -1) {
+      setPrev(false);
+      setRestart(false);
+    }
+    console.log(slideIndex)
 
-    }
   }
 
-  function prev(){
-    setSlideInd(slideind - 1);
-    setSlide(props.slides[slideind - 1]);
-    console.log(slideind);
-    if(slideind===1){
-      setPrevSlide(true)
-      setRestartSlide(true)
-      setNextSlide(false)
+  function restartApp() {
+    setSlideIndex(0);
+    setNext(false);
+    setPrev(true);
+    setRestart(true);
+    setSlide(props.slides[0]);
+  }
+  function prevSlide() {
+    setSlideIndex(slideIndex - 1);
+    setSlide(props.slides[slideIndex - 1]);
+    setNext(false);
+    if (slideIndex === 1) {
+      setPrev(true);
+      setRestart(true);
+      //setNext(false);
     }
   }
-  function restart(){
-    setPrevSlide(true)
-    setRestartSlide(true)
-    setNextSlide(false)
-    setSlide(props.slides[0])
-    setSlideInd(0)
+  console.log(slideIndex)
 
-  }
   return (
     <>
       <h1 data-testid="title">{slide.title}</h1>
       <p data-testid="text">{slide.text}</p>
-      <button data-testid="button-restart" onClick={() => restart()}
-         disabled={restartSlide}>
+      <button
+        data-testid="button-restart"
+        onClick={() => restartApp()}
+        disabled={restart}
+      >
         Restart
       </button>
-      <button data-testid="button-prev" onClick={() => prev()}
-             disabled={prevSlide}>
+      <button
+        data-testid="button-prev"
+        onClick={() => prevSlide()}
+        disabled={prev}
+      >
         Prev
       </button>
-      <button data-testid="button-next" 
-       onClick={() => next()}
-       disabled={nextSlide}>
+      <button
+        data-testid="button-next"
+        onClick={() => nextSlide()}
+        disabled={next}
+      >
         Next
       </button>
     </>
